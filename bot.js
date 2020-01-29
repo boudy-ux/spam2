2de9888b783c28be3164c3b5d2cc9cbd287e141f
boudy-ux <54374516+boudy-ux@users.noreply.github.com>
2020-01-28 21:55:57 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,12 +11,3 @@ channel.send(`اهلا ازيك عامل ايه كويس الحمدالله وا
 })
 
 client.login(process.env.BOT_TOKEN);
-client1.login(process.env.BOT_TOKEN1);
-client2.login(process.env.BOT_TOKEN2);
-client3.login(process.env.BOT_TOKEN3);
-client4.login(process.env.BOT_TOKEN4);
-client5.login(process.env.BOT_TOKEN5);
-client6.login(process.env.BOT_TOKEN6);
-client7.login(process.env.BOT_TOKEN7);
-client8.login(process.env.BOT_TOKEN8);
-client9.login(process.env.BOT_TOKEN9);

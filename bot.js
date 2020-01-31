4b63e1319c792e8acce0e6a878ad537d9e5c78cb
boudy-ux <54374516+boudy-ux@users.noreply.github.com>
2020-01-30 16:51:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,10 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("671953752772837381")
+let channel =     client.channels.get("672470572109660180")
 setInterval(function() {
 channel.send(`اهلا ازيك عامل ايه كويس الحمدالله وانتا عامل ايه بخير تمام الواحد مش لقي حاجه يعملها`);
-}, 10)
+}, 20)
 })
 
 client.login(process.env.BOT_TOKEN);
